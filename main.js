68aad16aeb66d6511e19fddd5e162fe0a737ccb3
sizanoskymaria <144914783+sizanoskymaria@users.noreply.github.com>
2025-06-23 08:41:36 -0300

    aula 05 23/06

--- main.js
+++ main.js
@@ -9,14 +9,30 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1 ){
-    tamanhoSenha = tamanhoSenha - 1;
+    tamanhoSenha--;
    } 
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 function aumentaTamanho(){
     if (tamanhoSenha < 20 ){
-     tamanhoSenha = tamanhoSenha +1;    
+     tamanhoSenha++;    
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha() {
+    let senha = ''; 
+    for(let i=0; i<tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas [numeroAleatorio];
+    }
+    campoSenha.value = letrasMaiusculas;
 }
