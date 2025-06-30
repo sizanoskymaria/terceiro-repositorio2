56445796d1dfaa2a1e8672a2767bbec831de76e7
sizanoskymaria <144914783+sizanoskymaria@users.noreply.github.com>
2025-06-30 08:23:39 -0300

    30/06

--- main.js
+++ main.js
@@ -24,11 +24,30 @@ function aumentaTamanho(){
 }
 
 const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
 geraSenha();
 
 function geraSenha() {
-    let senha = ''; 
+    let alfabeto = '';
+    if (checkbox [0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox [1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+
     for(let i=0; i<tamanhoSenha; i++) {
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
@@ -36,3 +55,4 @@ function geraSenha() {
     }
     campoSenha.value = senha;
 }
+
